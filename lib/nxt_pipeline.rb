3b9356fabf23252ba37826c8e57be3f5959d1204
Nils Sommer <mail@nilssommer.de>
2019-02-22 10:15:03 +0100

    bundle gem nxt_pipeline

---
+++ lib/nxt_pipeline.rb
@@ -0,0 +1,6 @@
+require "nxt_pipeline/version"
+
+module NxtPipeline
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/nxt_pipeline/version.rb
@@ -0,0 +1,3 @@
+module NxtPipeline
+  VERSION = "0.1.0"
+end
