55669c612a2b6c29eeb6c6ac399c205cf7f69df5
Andreas Robecke <a.robecke@getsafe.de>
2019-08-09 08:25:38 +0200

    Allow guard clauses for steps

--- lib/nxt_pipeline/step.rb
+++ lib/nxt_pipeline/step.rb
@@ -15,7 +15,14 @@ module NxtPipeline
     attr_reader :type, :result, :status, :error, :opts, :index
 
     def execute(arg)
-      self.result = constructor.call(self, arg)
+      guard_args = [arg, self]
+      if_guard_args = guard_args.take(if_guard.arity)
+      unless_guard_guard_args = guard_args.take(unless_guard.arity)
+
+      if unless_guard.call(*unless_guard_guard_args) && if_guard.call(*if_guard_args)
+        self.result = constructor.call(self, arg)
+      end
+
       set_status
       result
     rescue StandardError => e
@@ -36,6 +43,18 @@ module NxtPipeline
 
     attr_writer :result, :status, :error
     attr_reader :constructor
+
+    def if_guard
+      opts.fetch(:if) { no_guard }
+    end
+
+    def unless_guard
+      opts.fetch(:unless) { no_guard }
+    end
+
+    def no_guard
+      -> { true }
+    end
 
     def define_attr_readers(opts)
       opts.each do |key, value|
---
+++ spec/guard_clauses_spec.rb
@@ -0,0 +1,99 @@
+RSpec.describe NxtPipeline::Pipeline do
+  class StepOne
+    def initialize(opts)
+      @opts = opts
+    end
+
+    def call
+      @opts.upcase
+    end
+
+    def to_s
+      self.class.name
+    end
+  end
+
+  subject do
+    NxtPipeline::Pipeline.new do |pipeline|
+      pipeline.constructor(:service, default: true) do |step, arg|
+        step.service_class.new(arg).call
+      end
+    end
+  end
+
+  describe ':if guard' do
+    context 'when the guard takes no arguments' do
+      context 'and the guard claus applies' do
+        it 'skips the step' do
+          subject.execute('hanna') do |p|
+            p.step service_class: StepOne, if: -> { false }
+          end
+
+          expect(subject.logger.log.values.last).to eq(:skipped)
+        end
+      end
+    end
+
+    context 'when the guard takes a single arguments' do
+      context 'and the guard claus applies' do
+        it 'skips the step' do
+          subject.execute(false) do |p|
+            p.step service_class: StepOne, if: -> (arg) { arg }
+          end
+
+          expect(subject.logger.log.values.last).to eq(:skipped)
+        end
+      end
+    end
+
+    context 'when the guard takes two arguments' do
+      context 'and the guard claus applies' do
+        it 'skips the step' do
+          subject.execute(false) do |p|
+            p.step service_class: StepOne, if: -> (arg, step) { step.is_a?(NxtPipeline::Step) && arg }
+          end
+
+          expect(subject.logger.log.values.last).to eq(:skipped)
+        end
+      end
+    end
+  end
+
+  describe ':unless guard' do
+    context 'when the guard takes no arguments' do
+      context 'and the guard claus applies' do
+        it 'skips the step' do
+          subject.execute('hanna') do |p|
+            p.step service_class: StepOne, unless: -> { false }
+          end
+
+          expect(subject.logger.log.values.last).to eq(:skipped)
+        end
+      end
+    end
+
+    context 'when the guard takes a single arguments' do
+      context 'and the guard claus applies' do
+        it 'skips the step' do
+          subject.execute(false) do |p|
+            p.step service_class: StepOne, unless: -> (arg) { arg }
+          end
+
+          expect(subject.logger.log.values.last).to eq(:skipped)
+        end
+      end
+    end
+
+    context 'when the guard takes two arguments' do
+      context 'and the guard claus applies' do
+        it 'skips the step' do
+          subject.execute(false) do |p|
+            p.step service_class: StepOne, unless: -> (arg, step) { step.is_a?(NxtPipeline::Step) && arg }
+          end
+
+          expect(subject.logger.log.values.last).to eq(:skipped)
+        end
+      end
+    end
+  end
+end
