3d5d9edae322c386d280f021f69d268c903a1c78
Andreas Robecke <a.robecke@getsafe.de>
2019-08-22 18:45:28 +0200

    Extract step resolver specs

--- spec/pipeline_spec.rb
+++ spec/pipeline_spec.rb
@@ -519,45 +519,4 @@ RSpec.describe NxtPipeline::Pipeline do
       expect(subject).to eq('HANNA')
     end
   end
-
-  context 'dynamic arguments' do
-    subject do
-      NxtPipeline::Pipeline.new do |pipeline|
-        pipeline.step_resolver do |argument|
-          argument.is_a?(Class) && :service
-        end
-
-        pipeline.step_resolver do |argument|
-          argument.is_a?(String) && :dynamic
-        end
-
-        pipeline.constructor(:service, default: true) do |step, arg:|
-          result = step.argument.new(word: arg).call
-          { arg: result }
-        end
-
-        pipeline.constructor(:dynamic) do |step, arg:|
-          if step.argument == 'multiply'
-            { arg: arg * 2 }
-          elsif step.argument == 'symbolize'
-            { arg: arg.to_sym }
-          else
-            raise ArgumentError, "Don't know how to deal with argument: #{step.argument}"
-          end
-        end
-
-        pipeline.step StepOne
-        pipeline.step 'multiply'
-        pipeline.step 'symbolize'
-
-        pipeline.step do |step, arg:|
-          arg
-        end
-      end
-    end
-
-    it 'is possible to have dynamic arguments' do
-      expect(subject.execute(arg: 'hanna')).to eq(:HANNAHANNA)
-    end
-  end
 end
