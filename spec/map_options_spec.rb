79e672e1ff4c0a85fccd32038bf4f0ff0af124a1
Andreas Robecke <a.robecke@hellogetsafe.com>
2021-05-26 22:22:25 +0200

    Test multiple map_options

--- spec/map_options_spec.rb
+++ spec/map_options_spec.rb
@@ -1,19 +1,28 @@
 RSpec.describe NxtPipeline::Pipeline do
-  context 'mapped options' do
+  describe '#map_options' do
     subject do
       NxtPipeline::Pipeline.new do |pipeline|
         pipeline.constructor(:proc, default: true) do |step, opts|
           step.proc.call(opts, step.mapped_options)
         end
 
-        pipeline.step :proc,
-                      proc: ->(change_set, options) { change_set.merge(options) },
-                      map_options: ->(change_set) { { additional: 'options' } }
+        pipeline.step proc: ->(change_set, options) { change_set.merge(options) }, map_options: ->(change_set) { { additional: 'options' } }
+        pipeline.step proc: ->(change_set, options) { change_set.merge(options) }, map_options: ->(change_set) { { more_additional: 'options' } }
+        pipeline.step do |_, changeset|
+          changeset.merge(curried: 'options') # inline steps can also be used to map options
+        end
+        pipeline.step proc: ->(change_set, options) { change_set.merge(options) }, map_options: ->(change_set) { { even_more_additional: 'options' } }
       end
     end
 
     it 'passes the mapped options to the constructor' do
-      expect(subject.call(original: 'options')).to eq(original: 'options', additional: 'options')
+      expect(subject.call(original: 'options')).to eq(
+       original: 'options',
+       additional: 'options',
+       more_additional: 'options',
+       curried: 'options',
+       even_more_additional: 'options'
+      )
     end
   end
 end
